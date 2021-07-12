d5e30159da972031787083626cfd857b145f926c
Aye Daniel A <aadaniel108@gmail.com>
2021-07-12 12:56:34 +0100

    create UI class

--- main.js
+++ main.js
@@ -8,3 +8,58 @@ class Book {
     this.read = read;
   }
 }
+
+// UI Class: to handle UI Task
+
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      
+      <td><a href="#"><button class='btn btn-info readStatus' onclick="change(this)" >
+      ${book.read}</button></a></td>
+  
+      <td><a href="#" class="btn btn-danger btn-sm delete"> Delete</a></td>
+    
+      `;
+
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    // Vanish in 3seconds, after alert message shows
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#pages").value = "";
+    document.querySelector("#read").value = "";
+  }
+}
